4346183c367dc87ef05c4f7e5c435d50d899522c
sks <blueroof@jei.com>
2026-01-06 11:32:13 +0900

    오류2

--- sw.js
+++ sw.js
@@ -35,7 +35,7 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
+        caches.match(event.request, { ignoreSearch: true })
             .then((response) => {
                 return response || fetch(event.request);
             })
